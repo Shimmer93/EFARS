dd861da9e7991c08a5f6bb9cb11ab6ad181b36dc
Shimmer93 <zpengac@connect.ust.hk>
2021-10-20 23:21:11 +0800

    update human3.6m dataset

--- data/human36m.py
+++ data/human36m.py
@@ -5,39 +5,112 @@ import cv2 as cv
 from glob import glob
 import os
 
-class Human36M2dDataset(Dataset):
-    def __init__(self, img_dir, pose_dir):
+USED_JOINT_MASK = np.array([1,1,1,1,0,0,1,1,
+                            1,0,0,0,1,1,1,1,
+                            0,1,1,1,0,0,0,0,
+                            0,1,1,1,0,0,0,0],dtype=np.bool8)
+
+#CLASSES = []
+
+
+class Human36MBaseDataset(Dataset):
+    def __init__(self, img_dir, skeleton_2d_dir=None, skeleton_3d_dir=None, transforms=None, img_size=(256,256)):
         super().__init__()
-        self.img_fns = glob(os.path.join(img_dir, '*.jpg'))
-        np.random.shuffle(self.img_fns)
-        self.pose_dir = pose_dir
-        #self.mask = np.array([])
+        self.img_dir = img_dir
+        self.img_fns = None if img_dir is None else glob(os.path.join(img_dir, '*.jpg'))
+        self.skeleton_2d_dir = skeleton_2d_dir
+        self.skeleton_3d_dir = skeleton_3d_dir
+        self.transforms = transforms
+        self.img_size = img_size
 
     def __len__(self):
         return len(self.img_fns)
 
-    def __getitem__(self, index):
+    def _get_img_info(self, index):
         img_fn = self.img_fns[index]
-        fn_split = img_fn.split('_')
-        if len(fn_split) == 4:
-            action = fn_split[1] + '_' + fn_split[2]
-        else:
-            action = fn_split[1]
         img = cv.imread(img_fn)
-        pose_fn = os.path.join(self.pose_dir, fn_split[0].split('/')[-1], 'MyPoseFeatures/D2_Positions', action+'.npy')
-        poses = np.load(pose_fn)
-        pose = poses[int(fn_split[2].split('.')[0])//5]
+
+        fn_split = img_fn.split('_')
+        subset = fn_split[0].split('/')[-1]
+        action = fn_split[1] + ' ' + fn_split[2] if len(fn_split) == 4 else fn_split[1]
+        frame = int(fn_split[2].split('.')[0])
+
+        return img, subset, action, frame
         
+    def _prepare_img(self, img):
+        if self.img_size != None:
+            img = cv.resize(img, self.img_size, interpolation=cv.INTER_LINEAR)
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        img = torch.from_numpy(img.transpose((2, 0, 1)))
+        img = img.float() / 255.0
+        if self.transforms != None:
+            img = self.transforms(img)
+        return img
 
-        return img, pose
+    def _prepare_skeleton_2d(self, subset, action, frame, img_orig_size):
+        skeleton_2d_fn = os.path.join(self.skeleton_2d_dir, subset, 'MyPoseFeatures/D2_Positions', action + '.npy')
+        skeleton_2ds = np.load(skeleton_2d_fn)
+        skeleton_2d = skeleton_2ds[frame//5]
+        skeleton_2d = skeleton_2d[USED_JOINT_MASK,:]
+        skeleton_2d[:, 0] *= self.img_size[0] / img_orig_size[0]
+        skeleton_2d[:, 1] *= self.img_size[1] / img_orig_size[1]
+        
+        return torch.from_numpy(skeleton_2d)
+
+    # Further processing is needed for 3D skeletons
+    def _prepare_skeleton_3d(self, subset, action, frame, img_orig_size):
+        skeleton_3d_fn = os.path.join(self.skeleton_3d_dir, subset, 'MyPoseFeatures/D3_Positions', action.split('.')[0] + '.npy')
+        skeleton_3ds = np.load(skeleton_3d_fn)
+        skeleton_3d = skeleton_3ds[frame//5]
+        skeleton_3d = skeleton_3d[USED_JOINT_MASK,:]
+        skeleton_3d[:, 0] *= self.img_size[0] / img_orig_size[0]
+        skeleton_3d[:, 1] *= self.img_size[1] / img_orig_size[1]
+        return torch.from_numpy(skeleton_3d)
+
+class Human36M2DPoseDataset(Human36MBaseDataset):
+    def __init__(self, img_dir, skeleton_2d_dir, transforms=None, img_size=(256,256)):
+        super().__init__(img_dir=img_dir, skeleton_2d_dir=skeleton_2d_dir, transforms=transforms, img_size=img_size)
+
+    def __getitem__(self, index):
+        img, subset, action, frame = self._get_img_info(index)
+        w, h, _ = img.shape
+        img_orig_size = (w, h)
+        img = self._prepare_img(img)
+        skeleton_2d = self._prepare_skeleton_2d(subset, action, frame, img_orig_size)
+        return img, skeleton_2d
+
+class Human36M3DPoseDataset(Human36MBaseDataset):
+    def __init__(self, img_dir, skeleton_3d_dir, transforms=None, img_size=(256,256)):
+        super().__init__(img_dir=img_dir, skeleton_3d_dir=skeleton_3d_dir, transforms=transforms, img_size=img_size)
+
+    def __getitem__(self, index):
+        img, subset, action, frame = self._get_img_info(index)
+        w, h, _ = img.shape
+        img_orig_size = (w, h)
+        img = self._prepare_img(img)
+        skeleton_3d = self._prepare_skeleton_3d(subset, action, frame, img_orig_size)
+        return img, skeleton_3d
+
+class Human36M2DTo3DDataset(Human36MBaseDataset):
+    def __init__(self, img_dir, skeleton_2d_dir, skeleton_3d_dir, transforms=None, img_size=(256,256)):
+        super().__init__(img_dir=img_dir, skeleton_2d_dir=skeleton_2d_dir, skeleton_3d_dir=skeleton_3d_dir, transforms=transforms, img_size=img_size)
+
+    def __getitem__(self, index):
+        img, subset, action, frame = self._get_img_info(index)
+        w, h, _ = img.shape
+        img_orig_size = (w, h)
+        skeleton_2d = self._prepare_skeleton_2d(subset, action, frame, img_orig_size)
+        skeleton_3d = self._prepare_skeleton_3d(subset, action, frame, img_orig_size)
+        return skeleton_2d, skeleton_3d
 
 
-
-
-
+# TODO:
+# class Human36MUnsupervised3DDataset(Human36MBaseDataset): # For pose embedding
+# return 2 3D skeletons from the same video sequence with label "1" or from different video sequence with label "0"
 
 if __name__ == '__main__':
-    train_dataset = Human36M2dDataset('/scratch/PI/cqf/datasets/h36m/img', '/scratch/PI/cqf/datasets/h36m/pos2d')
-    img, pose = train_dataset[0]
+    train_dataset = Human36M2DPoseDataset('/scratch/PI/cqf/datasets/h36m/img', '/scratch/PI/cqf/datasets/h36m/pos2d')
+    img, skeleton = train_dataset[0]
     print(img.shape)
-    print(pose.shape)
+    print(skeleton.shape)
+    print(skeleton)
