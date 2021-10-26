a9f1ac4a44515248ee317cf3a47553d23d8c400c
Shimmer93 <zpengac@connect.ust.hk>
2021-10-26 15:00:41 +0800

    debug transforms

--- utils/transform.py
+++ utils/transform.py
@@ -17,16 +17,27 @@ def denormalize(img, mean, std):
 
 def do_pos2d_train_transforms(img, pts, crop_size, img_size, mean, std):
     max_pos = np.max(pts, axis=0)
-    min_pos = np.min(pts, axis=1)
-    min_w = np.maximum(int(max_pos[0] - crop_size[0]), 0)
-    min_h = np.maximum(int(max_pos[1] - crop_size[1]), 0)
-    max_w = np.minimum(int(min_pos[0]) + 1, img.shape[0] - crop_size[0]) + 1
-    max_h = np.minimum(int(min_pos[1]) + 1, img.shape[1] - crop_size[1]) + 1
-    rand_w = np.random.randint(min_w, max_w)
-    rand_h = np.random.randint(min_h, max_h)
+    min_pos = np.min(pts, axis=0)
+    print(f'min_pos: {min_pos}, max_pos: {max_pos}')
+    if max_pos[0] - min_pos[0] > crop_size[0] or max_pos[1] - min_pos[1] > crop_size[1]:
+        x_min = 0
+        y_min = 0
+        x_max = img.shape[0]
+        y_max = img.shape[1]
+    else:
+        x_min_min = np.maximum(int(max_pos[0] - crop_size[0]) + 1, 0)
+        y_min_min = np.maximum(int(max_pos[1] - crop_size[1]) + 1, 0)
+        x_min_max = np.minimum(int(min_pos[0]), img.shape[0] - crop_size[0])
+        y_min_max = np.minimum(int(min_pos[1]), img.shape[1] - crop_size[1])
+        x_min = np.random.randint(x_min_min, x_min_max)
+        y_min = np.random.randint(y_min_min, y_min_max)
+        x_max = x_min + crop_size[0]
+        y_max = y_min + crop_size[1]
 
+    print(f'x_min: {x_min}, y_min: {y_min}, x_max: {x_max}, y_max: {y_max}')
     transforms = A.Compose([
-        A.Crop(rand_w, rand_h, rand_w+crop_size[0], rand_h+crop_size[1], p=1),
+        A.Crop(x_min, y_min, x_max, y_max, p=1),
+        A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.9),
         A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.9),
         A.Blur(blur_limit=3,p=0.2),
         A.HorizontalFlip(p=0.5),
